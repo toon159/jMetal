8d89ad65941b4bfb3c573a8d661b35d825779cd5
Vichaya Raksakunpanich <toonbcc@gmail.com>
2019-05-01 14:07:43 +0700

    Fix problems and lists problem

--- jmetal-exec/src/main/java/org/uma/jmetal/runner/multiobjective/aaRunner.java
+++ jmetal-exec/src/main/java/org/uma/jmetal/runner/multiobjective/aaRunner.java
@@ -6,6 +6,7 @@ import org.uma.jmetal.runner.multiobjective.hNSGARunner;
 import org.uma.jmetal.runner.multiobjective.NSGAIIRunner;
 import org.uma.jmetal.runner.multiobjective.NSGAIIIRunner;
 import org.uma.jmetal.util.AbstractAlgorithmRunner;
+import org.uma.jmetal.util.JMetalLogger;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -23,10 +24,6 @@ public class aaRunner extends AbstractAlgorithmRunner {
             {"org.uma.jmetal.problem.multiobjective.dtlz.DTLZ2", "/pareto_fronts/DTLZ2.2D.pf", "300", "2", "false", "5"},
             {"org.uma.jmetal.problem.multiobjective.dtlz.DTLZ3", "/pareto_fronts/DTLZ3.2D.pf", "300", "2", "false", "5"},
             {"org.uma.jmetal.problem.multiobjective.dtlz.DTLZ4", "/pareto_fronts/DTLZ4.2D.pf", "300", "2", "false", "5"},
-            {"org.uma.jmetal.problem.multiobjective.dtlz.DTLZ1", "/pareto_fronts/DTLZ1.2D.pf", "300", "2", "false", "5"},
-            {"org.uma.jmetal.problem.multiobjective.dtlz.DTLZ2", "/pareto_fronts/DTLZ2.2D.pf", "300", "2", "false", "5"},
-            {"org.uma.jmetal.problem.multiobjective.dtlz.DTLZ3", "/pareto_fronts/DTLZ3.2D.pf", "300", "2", "false", "5"},
-            {"org.uma.jmetal.problem.multiobjective.dtlz.DTLZ4", "/pareto_fronts/DTLZ4.2D.pf", "300", "2", "false", "5"},
 //            {"org.uma.jmetal.problem.multiobjective.zdt.ZDT1", "/pareto_fronts/ZDT1.pf", "300", "3", "false", "5"},
 //            {"org.uma.jmetal.problem.multiobjective.zdt.ZDT2", "/pareto_fronts/ZDT2.pf", "300", "3", "false", "5"},
 //            {"org.uma.jmetal.problem.multiobjective.zdt.ZDT3", "/pareto_fronts/ZDT3.pf", "300", "3", "false", "5"},
@@ -35,18 +32,11 @@ public class aaRunner extends AbstractAlgorithmRunner {
             {"org.uma.jmetal.problem.multiobjective.dtlz.DTLZ2", "/pareto_fronts/DTLZ2.3D.pf", "300", "3", "false", "5"},
             {"org.uma.jmetal.problem.multiobjective.dtlz.DTLZ3", "/pareto_fronts/DTLZ3.3D.pf", "300", "3", "false", "5"},
             {"org.uma.jmetal.problem.multiobjective.dtlz.DTLZ4", "/pareto_fronts/DTLZ4.3D.pf", "300", "3", "false", "5"},
-            {"org.uma.jmetal.problem.multiobjective.dtlz.DTLZ1", "/pareto_fronts/DTLZ1.3D.pf", "300", "3", "false", "5"},
-            {"org.uma.jmetal.problem.multiobjective.dtlz.DTLZ2", "/pareto_fronts/DTLZ2.3D.pf", "300", "3", "false", "5"},
-            {"org.uma.jmetal.problem.multiobjective.dtlz.DTLZ3", "/pareto_fronts/DTLZ3.3D.pf", "300", "3", "false", "5"},
-            {"org.uma.jmetal.problem.multiobjective.dtlz.DTLZ4", "/pareto_fronts/DTLZ4.3D.pf", "300", "3", "false", "5"},
+
             {"org.uma.jmetal.problem.multiobjective.zdt.ZDT1", "/pareto_fronts/ZDT1.pf", "500", "2", "false", "5"},
             {"org.uma.jmetal.problem.multiobjective.zdt.ZDT2", "/pareto_fronts/ZDT2.pf", "500", "2", "false", "5"},
             {"org.uma.jmetal.problem.multiobjective.zdt.ZDT3", "/pareto_fronts/ZDT3.pf", "500", "2", "false", "5"},
             {"org.uma.jmetal.problem.multiobjective.zdt.ZDT4", "/pareto_fronts/ZDT4.pf", "500", "2", "false", "5"},
-            {"org.uma.jmetal.problem.multiobjective.dtlz.DTLZ1", "/pareto_fronts/DTLZ1.2D.pf", "500", "2", "false", "5"},
-            {"org.uma.jmetal.problem.multiobjective.dtlz.DTLZ2", "/pareto_fronts/DTLZ2.2D.pf", "500", "2", "false", "5"},
-            {"org.uma.jmetal.problem.multiobjective.dtlz.DTLZ3", "/pareto_fronts/DTLZ3.2D.pf", "500", "2", "false", "5"},
-            {"org.uma.jmetal.problem.multiobjective.dtlz.DTLZ4", "/pareto_fronts/DTLZ4.2D.pf", "500", "2", "false", "5"},
             {"org.uma.jmetal.problem.multiobjective.dtlz.DTLZ1", "/pareto_fronts/DTLZ1.2D.pf", "500", "2", "false", "5"},
             {"org.uma.jmetal.problem.multiobjective.dtlz.DTLZ2", "/pareto_fronts/DTLZ2.2D.pf", "500", "2", "false", "5"},
             {"org.uma.jmetal.problem.multiobjective.dtlz.DTLZ3", "/pareto_fronts/DTLZ3.2D.pf", "500", "2", "false", "5"},
@@ -59,22 +49,20 @@ public class aaRunner extends AbstractAlgorithmRunner {
             {"org.uma.jmetal.problem.multiobjective.dtlz.DTLZ2", "/pareto_fronts/DTLZ2.3D.pf", "500", "3", "false", "5"},
             {"org.uma.jmetal.problem.multiobjective.dtlz.DTLZ3", "/pareto_fronts/DTLZ3.3D.pf", "500", "3", "false", "5"},
             {"org.uma.jmetal.problem.multiobjective.dtlz.DTLZ4", "/pareto_fronts/DTLZ4.3D.pf", "500", "3", "false", "5"},
-            {"org.uma.jmetal.problem.multiobjective.dtlz.DTLZ1", "/pareto_fronts/DTLZ1.3D.pf", "500", "3", "false", "5"},
-            {"org.uma.jmetal.problem.multiobjective.dtlz.DTLZ2", "/pareto_fronts/DTLZ2.3D.pf", "500", "3", "false", "5"},
-            {"org.uma.jmetal.problem.multiobjective.dtlz.DTLZ3", "/pareto_fronts/DTLZ3.3D.pf", "500", "3", "false", "5"},
-            {"org.uma.jmetal.problem.multiobjective.dtlz.DTLZ4", "/pareto_fronts/DTLZ4.3D.pf", "500", "3", "false", "5"},
     };
 
     public static void main(String[] args) throws FileNotFoundException {
         int n = 30;
         ArrayList<double[]> list = new ArrayList();
         list.add(new double[] {2, 3, 2.5, 2.55});
-        double[] hv = new double[4];
         for (int i = 0; i < problemWithParetoFrontRef.length; i++) {
+            double[] hv = new double[4];
+
             for (int j = 0; j < n; j++) {
                 hv[0] += NSGAIIRunner.main(problemWithParetoFrontRef[i]);
                 hv[1] += NSGAIIIRunner.main(problemWithParetoFrontRef[i]);
                 hv[2] += hNSGARunner.main(problemWithParetoFrontRef[i]);
+
                 problemWithParetoFrontRef[i][5] = "-1";
                 hv[3] += hNSGARunner.main(problemWithParetoFrontRef[i]);
                 problemWithParetoFrontRef[i][5] = "5";
@@ -84,6 +72,7 @@ public class aaRunner extends AbstractAlgorithmRunner {
             hv[2]/=n;
             hv[3]/=n;
             list.add(hv);
+            JMetalLogger.logger.info(""+ (i+1) +" / " + problemWithParetoFrontRef.length + " (" + (i+1)  / problemWithParetoFrontRef.length + "%)");
         }
 
 
