48f4d72272a54ec43fd753c00eade3ca990941c3
Vichaya Raksakunpanich <toonbcc@gmail.com>
2020-02-15 17:09:50 +0700

    Add 3 algo to runner rewrite some code

--- jmetal-exec/src/main/java/org/uma/jmetal/runner/multiobjective/aaRunner.java
+++ jmetal-exec/src/main/java/org/uma/jmetal/runner/multiobjective/aaRunner.java
@@ -77,7 +77,7 @@ public class aaRunner extends AbstractAlgorithmRunner {
         int iter;
         ArrayList<double[]> list = new ArrayList();
         list.add(new double[]{});
-        int number_of_algo = 24;
+        int number_of_algo = 36;
 
         for (int max_gen = 0; max_gen < max_generation.length; max_gen++) {
             for (int i = 0; i < problemWithParetoFrontRef.length; i++) {
@@ -91,90 +91,153 @@ public class aaRunner extends AbstractAlgorithmRunner {
                         problemWithParetoFrontRef[i][5] = "" + (0.1 * k);
                         results[k] += aNSGARunner.main(problemWithParetoFrontRef[i]);
                     }*/
-                double[] nsga2, nsga3, ansga, nsga2hv, nsga3hv, ansgahv, nsga2igd, nsga3igd, ansgaigd;
+                double[] nsga2, nsga3, nsga31, nsga32, nsga33, nsga2hv, nsga3hv, nsga31hv, nsga32hv, nsga33hv, nsga2igd, nsga3igd, nsga31igd, nsga32igd, nsga33igd;
                 nsga2hv = new double[]{Double.MAX_VALUE, Double.MIN_VALUE};
                 nsga3hv = new double[]{Double.MAX_VALUE, Double.MIN_VALUE};
-                ansgahv = new double[]{Double.MAX_VALUE, Double.MIN_VALUE};
+                nsga31hv = new double[]{Double.MAX_VALUE, Double.MIN_VALUE};
+                nsga32hv = new double[]{Double.MAX_VALUE, Double.MIN_VALUE};
+                nsga33hv = new double[]{Double.MAX_VALUE, Double.MIN_VALUE};
                 nsga2igd = new double[]{Double.MAX_VALUE, Double.MIN_VALUE};
                 nsga3igd = new double[]{Double.MAX_VALUE, Double.MIN_VALUE};
-                ansgaigd = new double[]{Double.MAX_VALUE, Double.MIN_VALUE};
+                nsga31igd = new double[]{Double.MAX_VALUE, Double.MIN_VALUE};
+                nsga32igd = new double[]{Double.MAX_VALUE, Double.MIN_VALUE};
+                nsga33igd = new double[]{Double.MAX_VALUE, Double.MIN_VALUE};
 //                for (int j = 6; j < 18; j++) {
 //                    results[j] = 0;
 //                }
 
                 double[] hv_nsga2_list = new double[n];
                 double[] hv_nsga3_list = new double[n];
-                double[] hv_ansga_list = new double[n];
+                double[] hv_nsga31_list = new double[n];
+                double[] hv_nsga32_list = new double[n];
+                double[] hv_nsga33_list = new double[n];
                 double[] igd_nsga2_list = new double[n];
                 double[] igd_nsga3_list = new double[n];
-                double[] igd_ansga_list = new double[n];
+                double[] igd_nsga31_list = new double[n];
+                double[] igd_nsga32_list = new double[n];
+                double[] igd_nsga33_list = new double[n];
 
                 for (int j = 0; j < n; j++) {
 
                     nsga2 = NSGAIIRunner.main(problemWithParetoFrontRef[i]);
                     nsga3 = NSGAIIIRunner.main(problemWithParetoFrontRef[i]);
-                    ansga = aNSGARunner.main(problemWithParetoFrontRef[i]);
+                    nsga31 = NSGAIII1Runner.main(problemWithParetoFrontRef[i]);
+                    nsga32 = NSGAIII2Runner.main(problemWithParetoFrontRef[i]);
+                    nsga33 = NSGAIII3Runner.main(problemWithParetoFrontRef[i]);
 
                     hv_nsga2_list[j] = nsga2[0];
                     hv_nsga3_list[j] = nsga3[0];
-                    hv_ansga_list[j] = ansga[0];
+                    hv_nsga31_list[j] = nsga31[0];
+                    hv_nsga32_list[j] = nsga32[0];
+                    hv_nsga33_list[j] = nsga33[0];
                     igd_nsga2_list[j] = nsga2[1];
                     igd_nsga3_list[j] = nsga3[1];
-                    igd_ansga_list[j] = ansga[1];
+                    igd_nsga31_list[j] = nsga31[1];
+                    igd_nsga32_list[j] = nsga32[1];
+                    igd_nsga33_list[j] = nsga33[1];
 
                 }
-
-                DescriptiveStatistics da = new DescriptiveStatistics(hv_nsga2_list);
-                results[0] = da.getMean();
-                da = new DescriptiveStatistics(hv_nsga3_list);
-                results[1] = da.getMean();
-                da = new DescriptiveStatistics(hv_ansga_list);
-                results[2] = da.getMean();
-                da = new DescriptiveStatistics(igd_nsga2_list);
-                results[3] = da.getMean();
-                da = new DescriptiveStatistics(igd_nsga3_list);
-                results[4] = da.getMean();
-                da = new DescriptiveStatistics(igd_ansga_list);
-                results[5] = da.getMean();
-
-                da = new DescriptiveStatistics(hv_nsga2_list);
-                results[6] = da.getMin();
-                da = new DescriptiveStatistics(hv_nsga3_list);
-                results[7] = da.getMin();
-                da = new DescriptiveStatistics(hv_ansga_list);
-                results[8] = da.getMin();
-                da = new DescriptiveStatistics(igd_nsga2_list);
-                results[9] = da.getMin();
-                da = new DescriptiveStatistics(igd_nsga3_list);
-                results[10] = da.getMin();
-                da = new DescriptiveStatistics(igd_ansga_list);
-                results[11] = da.getMin();
-
-                da = new DescriptiveStatistics(hv_nsga2_list);
-                results[12] = da.getMax();
-                da = new DescriptiveStatistics(hv_nsga3_list);
-                results[13] = da.getMax();
-                da = new DescriptiveStatistics(hv_ansga_list);
-                results[14] = da.getMax();
-                da = new DescriptiveStatistics(igd_nsga2_list);
-                results[15] = da.getMax();
-                da = new DescriptiveStatistics(igd_nsga3_list);
-                results[16] = da.getMax();
-                da = new DescriptiveStatistics(igd_ansga_list);
-                results[17] = da.getMax();
-
-                da = new DescriptiveStatistics(hv_nsga2_list);
-                results[18] = da.getPercentile(75) - da.getPercentile(25);
-                da = new DescriptiveStatistics(hv_nsga3_list);
-                results[19] = da.getPercentile(75) - da.getPercentile(25);
-                da = new DescriptiveStatistics(hv_ansga_list);
-                results[20] = da.getPercentile(75) - da.getPercentile(25);
-                da = new DescriptiveStatistics(igd_nsga2_list);
-                results[21] = da.getPercentile(75) - da.getPercentile(25);
-                da = new DescriptiveStatistics(igd_nsga3_list);
-                results[22] = da.getPercentile(75) - da.getPercentile(25);
-                da = new DescriptiveStatistics(igd_ansga_list);
-                results[23] = da.getPercentile(75) - da.getPercentile(25);
+                DescriptiveStatistics da;
+                for (int j = 0; j <results.length; j++) {
+                    int mod10 = j%10;
+                    double[] l;
+//                    if(mod10 == 0){
+//                        l = hv_nsga2_list;
+//                    }
+                    switch(mod10){
+                        case 0:
+                            l = hv_nsga2_list; break;
+                        case 1:
+                            l = hv_nsga3_list; break;
+                        case 2:
+                            l = hv_nsga31_list; break;
+                        case 3:
+                            l = hv_nsga32_list; break;
+                        case 4:
+                            l = hv_nsga33_list; break;
+                        case 5:
+                            l = igd_nsga2_list; break;
+                        case 6:
+                            l = igd_nsga3_list; break;
+                        case 7:
+                            l = igd_nsga31_list; break;
+                        case 8:
+                            l = igd_nsga32_list; break;
+                        case 9:
+                            l = igd_nsga33_list; break;
+                        default:
+                            l = null;
+                    }
+                    da = new DescriptiveStatistics(l);
+                    if(j < 10){
+                        results[j] = da.getMean();
+                    } else if(j < 20){
+                        results[j] = da.getMin();
+                    } else if(j < 30){
+                        results[j] = da.getMin();
+                    } else {
+                        results[j] = da.getPercentile(75) - da.getPercentile(25);
+                    }
+                }
+//                da = new DescriptiveStatistics(hv_nsga2_list);
+//                results[0] = da.getMean();
+//                da = new DescriptiveStatistics(hv_nsga3_list);
+//                results[1] = da.getMean();
+//                da = new DescriptiveStatistics(hv_nsga31_list);
+//                results[2] = da.getMean();
+//                da = new DescriptiveStatistics(hv_nsga32_list);
+//                results[3] = da.getMean();
+//                da = new DescriptiveStatistics(hv_nsga33_list);
+//                results[4] = da.getMean();
+//                da = new DescriptiveStatistics(igd_nsga2_list);
+//                results[5] = da.getMean();
+//                da = new DescriptiveStatistics(igd_nsga3_list);
+//                results[6] = da.getMean();
+//                da = new DescriptiveStatistics(igd_nsga31_list);
+//                results[7] = da.getMean();
+//                da = new DescriptiveStatistics(igd_nsga32_list);
+//                results[8] = da.getMean();
+//                da = new DescriptiveStatistics(igd_nsga33_list);
+//                results[9] = da.getMean();
+//
+//                da = new DescriptiveStatistics(hv_nsga2_list);
+//                results[6] = da.getMin();
+//                da = new DescriptiveStatistics(hv_nsga3_list);
+//                results[7] = da.getMin();
+//                da = new DescriptiveStatistics(hv_ansga_list);
+//                results[8] = da.getMin();
+//                da = new DescriptiveStatistics(igd_nsga2_list);
+//                results[9] = da.getMin();
+//                da = new DescriptiveStatistics(igd_nsga3_list);
+//                results[10] = da.getMin();
+//                da = new DescriptiveStatistics(igd_ansga_list);
+//                results[11] = da.getMin();
+//
+//                da = new DescriptiveStatistics(hv_nsga2_list);
+//                results[12] = da.getMax();
+//                da = new DescriptiveStatistics(hv_nsga3_list);
+//                results[13] = da.getMax();
+//                da = new DescriptiveStatistics(hv_ansga_list);
+//                results[14] = da.getMax();
+//                da = new DescriptiveStatistics(igd_nsga2_list);
+//                results[15] = da.getMax();
+//                da = new DescriptiveStatistics(igd_nsga3_list);
+//                results[16] = da.getMax();
+//                da = new DescriptiveStatistics(igd_ansga_list);
+//                results[17] = da.getMax();
+//
+//                da = new DescriptiveStatistics(hv_nsga2_list);
+//                results[18] = da.getPercentile(75) - da.getPercentile(25);
+//                da = new DescriptiveStatistics(hv_nsga3_list);
+//                results[19] = da.getPercentile(75) - da.getPercentile(25);
+//                da = new DescriptiveStatistics(hv_ansga_list);
+//                results[20] = da.getPercentile(75) - da.getPercentile(25);
+//                da = new DescriptiveStatistics(igd_nsga2_list);
+//                results[21] = da.getPercentile(75) - da.getPercentile(25);
+//                da = new DescriptiveStatistics(igd_nsga3_list);
+//                results[22] = da.getPercentile(75) - da.getPercentile(25);
+//                da = new DescriptiveStatistics(igd_ansga_list);
+//                results[23] = da.getPercentile(75) - da.getPercentile(25);
 
                 list.add(results);
                 JMetalLogger.logger.info("max_Gen: " + max_generation[max_gen] + " => " + (iter + 1) + " / " + problemWithParetoFrontRef.length + " (" + Math.round((iter + 1.0) / problemWithParetoFrontRef.length * 100) + "%)");
